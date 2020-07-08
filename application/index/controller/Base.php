5c33353a5fe801e735fe3c9f867ca679db713723
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2020-07-08 10:19:48 +0800

    1. fix unsupported language.

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -2,9 +2,17 @@
 
 namespace app\index\controller;
 
+use think\App;
 use think\Controller;
-use think\Request;
+use think\facade\Lang;
 
 class Base extends Controller
 {
+    public function __construct(App $app = null)
+    {
+        parent::__construct($app);
+
+        // 设置允许的语言
+        Lang::setAllowLangList(['zh-cn','en-us']);
+    }
 }
