3f634a3bad37058aa91360880fb84b7c35639c17
LittleJake <465917717@qq.com>
2020-07-09 21:20:51 +0800

    1. disable multi-language due to thinkphp bug.

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -8,11 +8,4 @@ use think\facade\Lang;
 
 class Base extends Controller
 {
-    public function __construct(App $app = null)
-    {
-        parent::__construct($app);
-
-        // 设置允许的语言
-        Lang::setAllowLangList(['zh-cn','en-us']);
-    }
 }
