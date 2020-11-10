7fc6107bde63a1cdbf18c62db7872a2589d6f996
LittleJake <465917717@qq.com>
2020-11-10 13:16:26 +0800

    1. reuse code 2. add icon

--- application/common.php
+++ application/common.php
@@ -10,3 +10,16 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+function getIcon($desp = ''){
+    $url = "https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/";
+    $icon = ['redhat', 'centos', 'ubuntu', 'debian', 'windows', 'intel', 'amd', 'qemu', 'linux'];
+
+    foreach ($icon as $v)
+        if(stristr($desp, $v)) {
+            $url.="$v.svg";
+            return $url;
+        }
+
+
+    return $url."linux.svg";
+}
