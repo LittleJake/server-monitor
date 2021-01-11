adcd02458c11b866b4eb4efcb31be5dde35b47d6
LittleJake <465917717@qq.com>
2021-01-11 11:46:21 +0800

    1. add thermal 2. fix IP location invalid

---
+++ application/api/controller/Thermal.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace app\api\controller;
+
+
+use app\common\lib\SystemMonitor;
+use think\Controller;
+use think\Exception;
+use think\Response;
+
+class Thermal extends Controller
+{
+    protected $middleware = ['FlowControl', 'CheckToken'];
+
+    /**
+     * 显示资源列表
+     *
+     * @param string $token
+     * @return Response
+     * @throws Exception
+     */
+    public function get($token = '')
+    {
+        $ip = SystemMonitor::getIPByHash($token);
+
+        return json(SystemMonitor::collectionFormat(SystemMonitor::getThermalCollection($ip)));
+    }
+}
