a205107542eefb5b7a260a2f72cc29bff4b43368
LittleJake <13583702+LittleJake@users.noreply.github.com>
2024-02-07 22:03:33 +0800

    implement http API.

--- application/api/controller/Report.php
+++ application/api/controller/Report.php
@@ -5,20 +5,21 @@ namespace app\api\controller;
 use app\common\lib\SystemMonitor;
 use think\Controller;
 
+//TODO: Input sanitization.
 class Report extends Controller
 {
-    protected $middleware = ['FlowControl', 'CheckToken', 'CheckUUID', 'CheckJson'];
+    protected $middleware = ['FlowControl', 'CheckToken', 'CheckUUID' => ['except' => ['hash']]];
 
 
     public function collection($uuid = '')
     {
-        $json = $this-> request -> input();
-        return json(SystemMonitor::setCollection($uuid, $json));
+        $json = $this-> request -> post();
+        return json(SystemMonitor::setCollection($uuid, json_encode($json)));
     }
 
     public function info($uuid = '')
     {
-        $json = $this-> request -> input();
+        $json = $this-> request -> post();
         return json(SystemMonitor::setInfo($uuid, $json));
     }
 
