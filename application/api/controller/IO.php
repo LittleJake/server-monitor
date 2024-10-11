b5f08c06c947a712fbca6da04a3dec7e98d95a90
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2024-10-11 17:17:34 +0800

    Update
    1. Adding Disk I/O data
    2. update EChart to version 5.5.0
    3. Minor styles changed
    4. Reducing dup data

---
+++ application/api/controller/IO.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\lib\SystemMonitor;
+use think\Controller;
+use think\Exception;
+use think\Response;
+
+class IO extends Controller
+{
+    protected $middleware = ['FlowControl', 'CheckUUID', 'CheckJson'];
+
+    /**
+     * 显示资源列表
+     *
+     * @param string $uuid
+     * @return Response
+     * @throws Exception
+     */
+    public function get($uuid = '')
+    {
+        return json(SystemMonitor::collectionFormat(SystemMonitor::getCollection($uuid), "IO"));
+    }
+}
